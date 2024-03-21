eb27bd3c464fb136c051a086e3f8bd17577eb722
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-03-21 18:31:42 +0530

    Terraform AmiID

--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -9,14 +9,4 @@ variable "sample_string" {
 
 ## How to print the variable in Terraform below syntax"
 
-output "sample_string" {
-  value = var.sample_string
-}
 
-##In above case we just print the variable,If variable is a some combination of strings then the following is the syntax
-        ##${} is mandatory if we include the variable inside a string
-
-output "sample_string1" {
-
-   value = "value of sample_string = ${var.sample_string}"
-}
