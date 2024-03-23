0cc7c7b87306e90b5aa31b4c954b09fe27ffa1bb
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-03-23 11:34:43 +0530

    Terraform AmiID

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -1,3 +1,14 @@
 resource "null_resource" "null" {
   count = 10
 }
+
+resource "null_resource" "fruits" {
+
+  count = length(var.fruits)
+}
+
+
+variable "fruits"  {
+
+  default = ["apple", "mango", "banana",]
+}
