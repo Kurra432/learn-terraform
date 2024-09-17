55b38eb54c898e40ac3969f96e3cb3a75979f8f9
VEERA VENKATESWARARA <kurravenkatesh432@gmail.com>
2024-09-17 13:14:54 +0530

    loops in Terraform

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -4,6 +4,12 @@ resource "null_resource" "null" {
 
 resource "null_resource" "fruits" {
   count = length(var.fruits)
+
+  provisioner "local-exec" {
+
+    command = "echo fruit name - ${var.fruits[ count.index]}"
+
+  }
 }
 
 variable "fruits" {
